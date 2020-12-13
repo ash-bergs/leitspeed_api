8db3030f34d372dcacbc8e096b9dcdcf242fcc2f
Ash <ashley.bergsma42@gmail.com>
2020-12-12 21:24:55 -0700

    Gets server up and running

--- api/server.js
+++ api/server.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require("express"); 
 
 const server = express(); 
 
@@ -9,3 +9,7 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.status(200).json({ message: "Up and Running..."}); 
 })
+
+//! Gotcha! 
+// I forgot to export this module, and couldn't figure out why my server wasn't running! 
+module.exports = server;
