79f4ac2ab8667fc5e4ed7925620016aa9bb48b89
Harrison <hmseaborn@gmail.com>
2021-03-09 13:34:39 -0800

    idk

--- api/server.js
+++ api/server.js
@@ -1,23 +1,25 @@
-const express = require("express"); 
+const express = require("express");
 
-const cardsRouter = require('./users/cards-router'); 
-const usersRouter = require('./users/users-router'); 
-const topicsRouter = require('./users/topics-router'); 
-const server = express(); 
+const cardsRouter = require("./users/cards-router");
+const usersRouter = require("./users/users-router");
+const topicsRouter = require("./users/topics-router");
+const cors = require("cors");
+const server = express();
 
-//* use JSON // 
-server.use(express.json()); 
+//* use JSON //
+server.use(express.json());
+server.use(cors());
 
 //* "Plug in" the routers here
-server.use('/cards', cardsRouter);
-server.use('/users', usersRouter);  
-server.use('/topics', topicsRouter); 
+server.use("/cards", cardsRouter);
+server.use("/users", usersRouter);
+server.use("/topics", topicsRouter);
 
-//* base endpoint 
-server.get('/', (req, res) => {
-    res.status(200).json({ message: "Up and Running..."}); 
-})
+//* base endpoint
+server.get("/", (req, res) => {
+	res.status(200).json({ message: "Up and Running..." });
+});
 
-//! Gotcha! 
-// I forgot to export this module, and couldn't figure out why my server wasn't running! 
+//! Gotcha!
+// I forgot to export this module, and couldn't figure out why my server wasn't running!
 module.exports = server;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require('./api/server'); 
-// * bringing dotenv in 
-require('dotenv').config(); 
+const server = require("./api/server");
+// * bringing dotenv in
+require("dotenv").config();
 //* now switch this to the .env var, brought in by dotenv .config()
-const port = process.env.PORT; 
+const port = process.env.PORT || 5255;
 
 server.listen(port, () => console.log(`Server running on ${port}`));
