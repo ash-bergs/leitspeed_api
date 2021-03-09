84668aef9f4c0407efc772554e8be12faf8c29ea
Harrison <hmseaborn@gmail.com>
2021-03-09 14:03:28 -0800

    more noodles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const server = require("./api/server/server");
-// * bringing dotenv in
 require("dotenv").config();
+const server = require("./api/server");
+// * bringing dotenv in
+
 //* now switch this to the .env var, brought in by dotenv .config()
 const port = process.env.PORT || 5255;
 
