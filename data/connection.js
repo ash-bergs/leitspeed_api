03c1e0eded96be5c112c642a0a36c76de6bc5c77
Harrison <hmseaborn@gmail.com>
2021-03-09 14:30:18 -0800

    IDK

--- data/connection.js
+++ data/connection.js
@@ -3,7 +3,7 @@ const knex = require("knex");
 //* Imports the knex configuration we built before
 const config = require("../knexfile");
 
-const environment = process.env.NODE_ENV || "development";
+const environment = process.env.DB_ENV || "development";
 
 module.exports = knex(config[environment]);
 
--- knexfile.js
+++ knexfile.js
@@ -37,21 +37,21 @@ module.exports = {
 			directory: "./data/seeds",
 		},
 	},
-	staging: {
-		client: "postgresql",
-		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
-		},
-		pool: {
-			min: 2,
-			max: 10,
-		},
-		migrations: {
-			tableName: "knex_migrations",
-		},
-	},
+	// staging: {
+	// 	client: "postgresql",
+	// 	connection: {
+	// 		database: "my_db",
+	// 		user: "username",
+	// 		password: "password",
+	// 	},
+	// 	pool: {
+	// 		min: 2,
+	// 		max: 10,
+	// 	},
+	// 	migrations: {
+	// 		tableName: "knex_migrations",
+	// 	},
+	// },
 
 	production: {
 		client: "sqlite3",
