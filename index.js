8b2eda19c77c7dae8df0662eb0427e794b9db714
Harrison <hmseaborn@gmail.com>
2021-03-09 14:45:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
 const server = require("./api/server");
 // * bringing dotenv in
-
+require("dotenv").config();
 //* now switch this to the .env var, brought in by dotenv .config()
 const port = process.env.PORT || 5255;
 
