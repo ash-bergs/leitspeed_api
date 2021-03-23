31d5ebd001066509d4a130a4509d4781bd301044
Ash <ashley.bergsma42@gmail.com>
2021-03-23 11:56:55 -0700

    Adds passport to the project
    Passport and google-oauth20 were added through npm. I'm trying to set them up, but this requires more configuration with my developer API. Committing so that progress can be viewed.

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./api/server');
 // * bringing dotenv, using the config method it will bring in the declarations in finds in the `.env` file in the root of the project (not committed, private!)
 require('dotenv').config(); 
 //* now switch this to the .env variable, brought in by dotenv .config()
-const port = process.env.PORT; 
+const port = process.env.PORT || 3000; 
 
 server.listen(port, () => console.log(`Server running on ${port}`));
