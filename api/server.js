6023f9dd49d829a274235a4589a624aae1ae35a6
Harrison <hmseaborn@gmail.com>
2021-03-26 12:29:10 -0700

    cleaned up server file

--- api/server.js
+++ api/server.js
@@ -45,7 +45,6 @@ server.use(
 );
 
 // initialize is a function inside Passport library - see thread: https://stackoverflow.com/questions/46644366/what-is-passport-initialize-nodejs-express
-
 server.use(passport.initialize());
 //*google auth and local initialization ^^^^^
 // configures passport to use express-sessions config object
@@ -56,54 +55,6 @@ server.use("/auth", authRouter);
 server.use("/cards", cardsRouter);
 server.use("/users", usersRouter);
 server.use("/topics", topicsRouter);
-
-// adding to the spaghetti code with some shot-in-the-dark endpoints to test passport
-// GET register
-// //* the REGISTER end points seem to be working! a user is added with a hashed password
-// server.get("/register", (req, res) => {
-// 	res.render("register.ejs");
-// });
-
-server.post("/register", async (req, res) => {
-	const user = req.body;
-	const hashedPassword = await bcrypt.hash(req.body.password, 10);
-	user.password = hashedPassword;
-	users
-		.add(user)
-		.then((addedUser) => {
-			res.status(200).json(addedUser, "imatoken");
-		})
-		.catch((error) => {
-			res.status(500).json({ error: errror.message });
-		});
-	// try {
-	// 	const hashedPassword = await bcrypt.hash(req.body.password, 10);
-	// 	users.add({
-	// 		name: req.body.name,
-	// 		username: req.body.username,
-	// 		password: hashedPassword,
-	// 		email: req.body.email,
-	// 	});
-	// 	res.redirect("/login");
-	// } catch {
-	// 	res.redirect("/register");
-	// }
-});
-
-// //TODO now... to tackle login
-// server.get("/login", (req, res) => {
-// 	res.render("login.ejs");
-// });
-
-// we'll call passport to handle a login! see passport-config if curious what it's up to
-// server.post(
-// 	"/login",
-// 	passport.authenticate("local", {
-// 		successRedirect: "/",
-// 		failureRedirect: "/login",
-// 		failureFlash: true,
-// 	})
-// );
 
 //* base endpoint
 server.get("/", (req, res) => {
